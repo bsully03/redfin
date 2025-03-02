5aa652f5583fea16272ef86c5de8a2552d8d353e
Brian Sullivan <briansullivan@Mac.attlocal.net>
2025-03-02 13:21:09 -0500

    add cookies

--- redfin/redfin.py
+++ redfin/redfin.py
@@ -1,13 +1,33 @@
 import requests
 import json
-
+import browser_cookie3
 
 class Redfin:
     def __init__(self):
         self.base = 'https://redfin.com/stingray/'
         self.user_agent_header = {
-            'user-agent': 'redfin'
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'accept-language': 'en-US,en;q=0.9',
+            'cookie': self.load_chrome_cookies('redfin.com'),
+            'priority': 'u=0, i',
+            'sec-ch-ua': '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"macOS"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'none',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36'
         }
+        print("here")
+
+    def load_chrome_cookies(self, domain):
+        """Loads Chrome cookies for a specific domain."""
+        cj = browser_cookie3.chrome()
+        cookies = [
+            f'{cookie.name}={cookie.value}' for cookie in cj if domain in cookie.domain]
+        return ';'.join(cookies)
 
     def meta_property(self, url, kwargs, page=False):
         if page:
