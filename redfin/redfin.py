12fe09d4d1ca98b02660ddabcfe55f6634b871c6
Brian Sullivan <briansullivan@Mac.attlocal.net>
2025-03-02 19:44:57 -0500

    tweak

--- redfin/redfin.py
+++ redfin/redfin.py
@@ -20,7 +20,6 @@ class Redfin:
             'upgrade-insecure-requests': '1',
             'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36'
         }
-        print("here")
 
     def load_chrome_cookies(self, domain):
         """Loads Chrome cookies for a specific domain."""
